70307661398ce445225de5361bae6e540e76e5ec
unknown <ivancampos039@gmail.com>
2022-07-28 21:48:53 -0300

    primeiro projeto em React

---
+++ 05-create-react-app/primeiro-app/src/components/card/card.js
@@ -0,0 +1,26 @@
+import React from "react"
+import "./card.css"
+
+
+const Card = ({title}) => {
+    return(
+        <div className="card">
+            <h3>{title}</h3>
+            <p>esse é um texto de card</p>
+         </div>
+    )
+}
+/*
+class Card extends React.Component {
+    render() {
+        
+        return (
+            <div className="card">
+                <h3>{this.props.title}</h3>
+                <p>esse é um texto de card</p>
+            </div>
+        )
+    }
+}*/
+
+export default Card;
